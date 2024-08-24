aad615abc97552cfe4a2c8dee24a9994fc1cf2c9
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-25 00:50:22 +0300

    add the identity context seed

---
+++ UserService/Data/IdentityContextSeed.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Microsoft.AspNetCore.Identity;
+using System.Text.Json;
+using WebApplication1.Models;
+
+namespace Repository.Identity
+{
+    public class IdentityContextSeed
+    {
+        public static async Task SeedUsersAsync(UserManager<AppUser> _userManager)
+        {
+            if (_userManager.Users.Count() == 0)
+            {
+                var usersData = File.ReadAllText("../Repository/Identity/DataSeeding/users.json");
+                var users = JsonSerializer.Deserialize<List<AppUser>>(usersData);
+
+                if (users?.Count() > 0)
+                {
+                    foreach (var user in users)
+                    {
+                        await _userManager.CreateAsync(user, "Pa$$w0rd");
+                    }
+                }
+            }
+        }
+    }
+}
