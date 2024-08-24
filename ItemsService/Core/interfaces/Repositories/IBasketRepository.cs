cc87c02e4a5a0a77c50bf873cc5bbcfc447779ee
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-24 17:47:23 +0300

    add repositories

---
+++ ItemsService/Core/interfaces/Repositories/IBasketRepository.cs
@@ -0,0 +1,13 @@
+﻿
+using OrdersAndItemsService.Core.Models.BasketEntites;
+
+namespace OrdersAndItemsService.Core.interfaces.Repositories
+{
+    public interface IBasketRepository
+    {
+
+        Task<Basket?> CreateOrUpdateBasketAsync(Basket basket);
+        Task<Basket?> GetBasketAsync(string basketId);
+        Task<bool> DeleteBasketAsync(string basketId);
+    }
+}
---
+++ ItemsService/Core/interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities;
+using OrdersAndItemsService.Core.Models;
+
+namespace OrdersAndItemsService.Core.interfaces.Repositories
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IGenericRepository<T> Repository<T>() where T : BaseEntity;
+        Task<int> CompleteAsync();
+    }
+}
