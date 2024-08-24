1111327b483fae181893de580f3c3f257331fa65
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-25 00:47:07 +0300

    modify the globals namespaces

--- ItemsService/Glopals.cs
+++ ItemsService/Glopals.cs
@@ -5,6 +5,6 @@ global using  System.ComponentModel.DataAnnotations;
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.EntityFrameworkCore;
 global using System.Text.Json;
-
-
+global using Microsoft.AspNetCore.Authorization;
+global using System.Net;
 
