c3abb0cc480470683f73788ae8b47142b5800124
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-25 00:48:40 +0300

    add account controller

---
+++ UserService/Controllers/AccountController.cs
@@ -0,0 +1,142 @@
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using OrdersAndItemsService.API.Errors;
+using OrdersAndItemsService.Controllers;
+using System.Security.Claims;
+using Address = Models.Address;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IAuthService _authService;
+
+        public AccountController(UserManager<AppUser> userManager, IMapper mapper,
+            SignInManager<AppUser> signInManager, IAuthService authService)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+            _signInManager = signInManager;
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        [ProducesResponseType(typeof(AppUserDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<AppUserDto>> Login(LoginDto model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if (user is null)
+                return Unauthorized(new ApiResponse(401));
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+
+            if (result.Succeeded is false)
+                return Unauthorized(new ApiResponse(401));
+
+            return Ok(new AppUserDto
+            {
+                DisplayName = user.DisplayName,
+                Email = model.Email,
+                Token = await _authService.CreateTokenAsync(user, _userManager)
+            });
+        }
+
+        [HttpPost("register")]
+        [ProducesResponseType(typeof(AppUserDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<AppUserDto>> Register(RegisterDto model)
+        {
+            if (CheckEmailExist(model.Email).Result.Value)
+                return BadRequest(new ApiValidationErrorResponse() { Errors = new string[] { "This email has already been used" } });
+
+            var user = new AppUser()
+            {
+                DisplayName = model.DisplayName,
+                Email = model.Email,
+                UserName = model.Email.Split('@')[0],
+                PhoneNumber = model.PhoneNumber,
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded is false)
+                return BadRequest(new ApiResponse(400));
+
+            return Ok(new AppUserDto
+            {
+                DisplayName = user.DisplayName,
+                Email = model.Email,
+                Token = await _authService.CreateTokenAsync(user, _userManager)
+            });
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<AppUserDto>> GetCurrentUser()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var user = await _userManager.FindByEmailAsync(email);
+            return Ok(new AppUserDto()
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Token = await _authService.CreateTokenAsync(user, _userManager)
+            });
+        }
+
+        [Authorize]
+        [HttpGet("address")]
+        public async Task<ActionResult<OrderAddressDto>> GetCurrentUserAddress()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+
+            //var user = await _userManager.FindByEmailAsync(email);
+            // -- we don't use it because it load user without navigational property so I created extension method do that
+
+            var user = await _userManager.Users.Include(x => x.Address).SingleOrDefaultAsync(u => u.Email == email);
+
+            return Ok(_mapper.Map<Address, OrderAddressDto>(user.Address));
+        }
+
+        [Authorize]
+        [HttpPut("address")]
+        [ProducesResponseType(typeof(OrderAddressDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<OrderAddressDto>> UpdateUserAddress(AddressDto updatedAddress)
+        {
+            var address = _mapper.Map<AddressDto, Address>(updatedAddress);
+
+            var email = User.FindFirstValue(ClaimTypes.Email);
+
+            var user = await _userManager.Users.Include(x => x.Address).SingleOrDefaultAsync(u => u.Email == email);
+
+            updatedAddress.Id = user.Address.Id;
+
+            user.Address = address;
+
+            var result = await _userManager.UpdateAsync(user);
+
+            if (!result.Succeeded)
+                return BadRequest(new ApiResponse(400));
+
+            return Ok(updatedAddress);
+        }
+
+        [HttpGet("emailexists")]
+        public async Task<ActionResult<bool>> CheckEmailExist(string email)
+        {
+            return await _userManager.FindByEmailAsync(email) is not null;
+        }
+    }
+}
