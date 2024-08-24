c44ee30ec98ee916087b4d859900f436e714805e
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-25 00:33:31 +0300

    organize dtos

---
+++ ItemsService/API/Dtos/BasketItemDto.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace API.Dtos
+{
+    public class BasketItemDto
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        [Required]
+        [Range(0.1, double.MaxValue, ErrorMessage = "invalid price!")]
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Brand { get; set; }
+
+        [Required]
+        [Range(0.1, double.MaxValue, ErrorMessage = "invalid quantity!")]
+        public int Quantity { get; set; }
+    }
+}
