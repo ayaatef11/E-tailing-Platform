0bc0d7aea3fefe6099a8c705943001d41cb90e7f
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-24 17:42:54 +0300

    update global file

--- ItemsService/Glopals.cs
+++ ItemsService/Glopals.cs
@@ -1,7 +1,10 @@
 ﻿global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Mvc;
-global using WebApplication1.Models;
-
-
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using  System.ComponentModel.DataAnnotations;
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.EntityFrameworkCore;
+global using System.Text.Json;
+
+
+
