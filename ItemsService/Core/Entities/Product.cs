01a22efaa7b4ee08ed4cb7ff4232b729615858fa
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-25 00:32:33 +0300

    add product entity

--- ItemsService/Core/Entities/Product.cs
+++ ItemsService/Core/Entities/Product.cs
@@ -1,6 +1,6 @@
 ﻿
 
-namespace OrdersAndItemsService.Core.Models
+namespace OrdersAndItemsService.Core.Entities
 {
     public class Product : BaseEntity
     {
@@ -9,9 +9,9 @@ namespace OrdersAndItemsService.Core.Models
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string PictureUrl { get; set; }
-        public int BrandId { get; set; } // FK - ProductBrand - But We Don't Named ProductBrandId So EF Don't Know This FK So We Make It In Fluent API
-        public ProductBrand Brand { get; set; } // Navigational Property
-        public int CategoryId { get; set; } // FK - ProductCategory - But We Don't Named ProductCategoryId So EF Don't Know This FK So We Make It In Fluent API
-        public ProductCategory Category { get; set; } // Navigational Property
+        public int BrandId { get; set; }//navigation for product brand and we make it in fluent api 
+        public ProductBrand Brand { get; set; }
+        public int CategoryId { get; set; }  
+        public ProductCategory Category { get; set; } 
     }
 }
