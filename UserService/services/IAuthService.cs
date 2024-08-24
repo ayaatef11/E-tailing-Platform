59cfc395984e73f4007cd9316098d4a7046036ad
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-25 00:52:01 +0300

    add the auth service class

---
+++ UserService/services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using WebApplication1.Models;
+
+namespace Core.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        Task<string> CreateTokenAsync(AppUser user, UserManager<AppUser> userManager);
+    }
+}
