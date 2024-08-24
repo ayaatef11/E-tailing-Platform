453904d3b32674a2cf8dcb27236f93ec00464009
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-24 17:39:43 +0300

    update basket repo

--- ItemsService/Repository/Repositories/BasketRepository.cs
+++ ItemsService/Repository/Repositories/BasketRepository.cs
@@ -1,8 +1,7 @@
-﻿using OrdersAndItemsService.Core.Models;
-using OrdersAndItemsService.Core.Models.BasketEntites;
-using OrdersAndItemsService.Repository.IRepositories;
+﻿using OrdersAndItemsService.Core.Entities.BasketEntites;
+using OrdersAndItemsService.Core.interfaces.Repositories;
 using StackExchange.Redis;
-using System.Text.Json;
+
 namespace OrdersAndItemsService.Repository.Repositories
 {
     public class BasketRepository : IBasketRepository
@@ -15,10 +14,14 @@ namespace OrdersAndItemsService.Repository.Repositories
         }
         public async Task<Basket?> CreateOrUpdateBasketAsync(Basket basket)
         {
+            // Set a string value for the given key.
             var createdOrUpdated = await _database.StringSetAsync(basket.Id, JsonSerializer.Serialize(basket), TimeSpan.FromDays(30));
             if (createdOrUpdated is false) return null;
             return await GetBasketAsync(basket.Id);
         }
+
+      
+
         public async Task<bool> DeleteBasketAsync(string basketId)
         {
             return await _database.KeyDeleteAsync(basketId);
@@ -26,18 +29,10 @@ namespace OrdersAndItemsService.Repository.Repositories
 
         public async Task<Basket?> GetBasketAsync(string basketId)
         {
-            var basket = await _database.StringGetAsync(basketId);
+            var basket = await _database.StringGetAsync(basketId);// get a string value for the given key.
             return basket.IsNullOrEmpty ? null : JsonSerializer.Deserialize<Basket>(basket);
         }
 
-        public Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<CustomerBasket> IBasketRepository.GetBasketAsync(string basketId)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
