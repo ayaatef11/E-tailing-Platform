b9936d392ebdb37bbf5056352cc18bb6205e5d6a
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-25 00:49:57 +0300

    add the identity context

---
+++ UserService/Data/IdentityContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Stripe;
+using UserService.Models;
+using WebApplication1.Models;
+
+namespace UserService.Data
+{
+    public class IdentityContext : IdentityDbContext<AppUser>
+    {
+        public DbSet<RefreshTokens> RefreshTokens { get; set; }
+
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options) { }
+        // In this method we override OnModelCreating which exist in base class
+        // so we need to call it
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Address>().ToTable("Addresses");
+        }
+
+
+        }
+    }
