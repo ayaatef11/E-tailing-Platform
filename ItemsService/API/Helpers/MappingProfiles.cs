35c3246c9022eb5b84de5a7574cfaac706be96fa
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-25 00:34:15 +0300

    add helper classes

---
+++ ItemsService/API/Helpers/MappingProfiles.cs
@@ -0,0 +1,50 @@
+﻿
+using API.Dtos;
+using AutoMapper;
+using OrdersAndItemsService.Core.Entities;
+using OrdersAndItemsService.Core.Entities.BasketEntites;
+using OrdersAndItemsService.Core.Entities.OrderEntities;
+
+
+
+namespace API.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        //  public IConfiguration Configuration { get; }
+
+       // Static Mapping: If all your mappings are static and do not depend on external configuration, you do not need IConfiguration.In this case, your current approach, where you define mappings directly in the constructor, is sufficient.
+        //Simple Mapping: For straightforward mappings, such as the ones defined in your MappingProfiles class, IConfiguration is not necessary unless you plan to extend it to include configuration-based logic.
+        public MappingProfiles()//constructor
+        {
+            CreateMap<Product, ProductToReturnDto>()
+            .ForMember(F => F.Brand, C => C.MapFrom(S => S.Brand.Name))
+            .ForMember(F => F.Category, C => C.MapFrom(S => S.Category.Name))
+            .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductPictureUrlResolver>());
+            CreateMap<BasketDto, Basket>();
+            CreateMap<BasketItemDto, BasketItem>();
+
+            CreateMap<Basket, BasketToReturnDto>();
+
+            CreateMap<BasketItem, BasketItemToReturnDto>()
+           .ForMember(d => d.PictureUrl, o => o.MapFrom<BasketItemPictureUrlResolver>());
+
+          
+
+            CreateMap<OrderAddressDto, OrderAddress>();
+
+            CreateMap<Order, OrderToReturnDto>()
+                .ForMember(d => d.DeliveryMethod, o => o.MapFrom(s => s.DeliveryMethod.Name))
+                .ForMember(d => d.DeliveryMethodCost, o => o.MapFrom(s => s.DeliveryMethod.Cost));
+
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(d => d.ProductId, o => o.MapFrom(s => s.Product.ProductId))
+                .ForMember(d => d.ProductName, o => o.MapFrom(s => s.Product.ProductName))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.Product.PictureUrl))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<OrderItemPictureUrlResolver>());
+
+        }
+
+       
+    }
+}
---
+++ ItemsService/API/Helpers/ProductPictureUrlResolver.cs
@@ -0,0 +1,26 @@
+﻿
+using API.Dtos;
+using AutoMapper;
+using OrdersAndItemsService.Core.Entities;
+
+namespace API.Helpers
+{
+    public class ProductPictureUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProductPictureUrlResolver(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return $"{_configuration["ApiBaseUrl"]}/{source.PictureUrl}";
+            }
+            return string.Empty;
+        }
+    }
+}
