44d59b4c1fe9c2f453792d4bd9e961ca04b06a12
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-25 00:39:21 +0300

    add the app user

--- UserService/Models/AppUser.cs
+++ UserService/Models/AppUser.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
-namespace WebApplication1.Models
+namespace Models
 {
     public class AppUser:IdentityUser
     {   
         public int Id { get; set; }
-        public string Name { get; set; }=string.Empty;
+        public string DisplayName { get; set; }=string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
-        public List<Order>?orders { get; set; }
+        public Address Address { get; set; }
 
 
     }
