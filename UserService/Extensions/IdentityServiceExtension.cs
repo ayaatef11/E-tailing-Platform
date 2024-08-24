568485c5ba43556dcdb3ac15890c3c64519acf78
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-25 01:03:28 +0300

    add the identity service extension

--- UserService/Extensions/IdentityServiceExtension.cs
+++ UserService/Extensions/IdentityServiceExtension.cs
@@ -4,7 +4,7 @@ using WebApplication1.Models;
 
 namespace UserService.Extensions
 {
-    public class IdentityServiceExtension
+    public static  class IdentityServiceExtension
     {
         public static IServiceCollection AddIdentityServices(this IServiceCollection services)
         {
