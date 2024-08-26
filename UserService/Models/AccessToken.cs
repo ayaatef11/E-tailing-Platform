62b1e380c8287eb48e715a0249a48b3649f9b426
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-26 20:17:36 +0300

    add the access token class

---
+++ UserService/Models/AccessToken.cs
@@ -0,0 +1,13 @@
+﻿namespace UserService.Models
+{
+    public class AccessToken
+   {
+            public string Token { get; set; }
+            public DateTime Expiry { get; set; }
+            public string TokenType { get; set; }
+            public string Scope { get; set; }
+            public bool IsExpired() => DateTime.UtcNow >= Expiry;
+        }
+
+    }
+
