241a9512ea61b0897cf252f772968cf79e41322a
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-26 20:18:37 +0300

    add other global packages

---
+++ UserService/Globals.cs
@@ -0,0 +1,14 @@
+﻿global using System.Text.Json;
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Text;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Net.Mail;
+global using System.Net;
+global using AutoMapper;
+
