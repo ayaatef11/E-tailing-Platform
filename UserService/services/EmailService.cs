932ce0c69ab0ebdd6b441f8bd95de73ac118ea9b
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-26 20:16:34 +0300

    add email service

---
+++ UserService/services/EmailService.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace UserService.services
+{
+    public class EmailService(IConfiguration _configuration) :IEmailService
+    {
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            var smtpSettings = _configuration.GetSection("EmailSettings");
+
+            using var smtpClient = new SmtpClient(smtpSettings["SmtpServer"], int.Parse(smtpSettings["SmtpPort"]!))
+            {
+                Credentials = new NetworkCredential(smtpSettings["SenderEmail"], smtpSettings["SenderPassword"]),
+               // EnableSsl = true
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(smtpSettings["SenderEmail"]!),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+
+            mailMessage.To.Add(toEmail);
+
+            await smtpClient.SendMailAsync(mailMessage);
+        }
+
+
+    }
+}
