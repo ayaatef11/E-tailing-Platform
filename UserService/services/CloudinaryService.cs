7286ba0afcc3b81e50be904d83c8c3c914e72fa2
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-29 19:10:09 +0300

    resolve namespaces in cloudinary settings

---
+++ UserService/services/CloudinaryService.cs
@@ -0,0 +1,29 @@
+﻿using CloudinaryDotNet.Actions;
+using CloudinaryDotNet;
+
+namespace UserService.services
+{
+    public class CloudinaryService : IPhotoService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public CloudinaryService()
+        {
+            var account = new Account(
+               //configurations for your account to load the images 
+            );
+            _cloudinary = new Cloudinary(account);
+        }
+
+        
+        public async Task<ImageUploadResult> UploadImageAsync(IFormFile file)
+        {
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(file.FileName, file.OpenReadStream())
+            };
+            return await _cloudinary.UploadAsync(uploadParams);
+        }
+
+    }
+}
