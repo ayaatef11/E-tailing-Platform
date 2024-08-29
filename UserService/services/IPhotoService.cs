eb8895ca99136b87375b440a1d824f362e0dd2ba
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-29 19:11:20 +0300

    add photo service interface

---
+++ UserService/services/IPhotoService.cs
@@ -0,0 +1,9 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace UserService.services
+{
+    public interface IPhotoService
+    {
+        public Task<ImageUploadResult> UploadImageAsync(IFormFile file);
+    }
+}
