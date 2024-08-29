2682a26966035e5e8be3cc833020498c53dc13dc
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-29 19:10:39 +0300

    create token interface

---
+++ UserService/services/ITokenService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Models;
+using Models.DTOS.Requests;
+using UserService.DTOs.Configuration;
+
+namespace UserService.services
+{
+    public interface ITokenService
+    {
+        Task<string> CreateTokenAsync(AppUser user, UserManager<AppUser> userManager);
+        // void createRole(string roleName);
+        Task<bool> AssignRoleToUser(string userEmail, string roleName);
+        TokenModel GenerateTokens(ClaimsIdentity identity);
+        string GenerateAccessToken(ClaimsIdentity identity);
+
+        JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims);
+        Task<AuthResult> GenerateJwtToken(AppUser user);
+        Task<AuthResult> VerifyToken(TokenRequests tokenRequest);
+    }
+}
