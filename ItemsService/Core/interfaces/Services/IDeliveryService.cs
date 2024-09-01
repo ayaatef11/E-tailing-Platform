feef91b4956115be5293131cdf5a195b44b92fcf
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-02 00:01:54 +0300

    apply delivery and invoice

---
+++ ItemsService/Core/interfaces/Services/IDeliveryService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Core.Entities.OrderEntities;
+
+namespace Core.interfaces.Services
+{
+    public  interface IDeliveryService
+    {
+        Task<IReadOnlyList<OrderDeliveryMethod>> GetAllDeliveryMethodsAsync();
+    }
+}
---
+++ ItemsService/Core/interfaces/Services/IInvoiceService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.interfaces.Services
+{
+    public  interface  IInvoiceService
+    {
+      /*  Task<IReadOnlyList<Invoice>> GetAllInvoicesAsync();
+        Task<IReadOnlyList<Invoice>> GetInvoiceByIdUsingSpecAsync(int id);*/
+    }
+}
