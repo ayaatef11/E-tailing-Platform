489791c20062d2d48ba0d71a75dd397c9edd6775
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-01 23:59:11 +0300

    add delivery

---
+++ ItemsService/API/Controllers/DeliveryController.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.OrderEntities;
+using Core.interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeliveryController(IOrderService orderService) : ControllerBase
+    {
+        [HttpGet("deliveryMethod")]
+        public async Task<ActionResult<IReadOnlyList<OrderDeliveryMethod>>> GetAllDeliveryMethods()
+        {
+            var deliveryMethods = await orderService.GetAllDeliveryMethodsAsync();
+
+            return Ok(deliveryMethods);
+        }
+    }
+}
